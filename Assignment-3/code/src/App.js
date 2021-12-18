04f0ce0e1bc2cc82bc11d9be058f6a5bb135c743
Sameer Khan <samarir786@gmail.com>
2021-12-18 18:26:22 +0530

    check_commit

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './header.js';
+import customer_info from './customer-info';
+import order_info from './order-info';
+import product_list from './product-list';
+import './App.css'
+
 
 
 var arrow="<"
+
 function App() {
+  const desk = {
+    Name: "Sameer khan",
+    employeeid: "696969",
+    email: "1213@gmail.com",
+    phone: "8384848432",
+    appointment:"tommorrow"
+  };
   return (
     <div className="site-container">
-    {/* Start Your code here */}
-    </div>
+      <div className='head'>
+
+        <div className='arr'>{arrow}</div>
+        <span className='print'>print</span>
+        <div className='details'>{desk.Name}<br></br>{desk.employeeid} </div>
+        
+      </div>
+     </div>
   );
 }
 
