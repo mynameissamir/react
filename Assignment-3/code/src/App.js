f47884ac6996b87b0b65aab14a2b1b5877dd3111
Sameer Khan <samarir786@gmail.com>
2021-12-19 14:40:49 +0530

    commit1

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import Header from './header.js';
-import customer_info from './customer-info';
+import Customer_info from './customer-info';
 import order_info from './order-info';
 import product_list from './product-list';
 import './App.css'
+import unchecked from './unchecked.png';
 
 
-
-var arrow="<"
+var arrow="<";
 
 function App() {
   const desk = {
     Name: "Sameer khan",
     employeeid: "696969",
-    email: "1213@gmail.com",
+    Email: "1213@gmail.com",
     phone: "8384848432",
-    appointment:"tommorrow"
+    appointment:new Date(645556).toLocaleString(),
+    status:"In Progress",
+    door:"Mark"
+    
   };
   return (
     <div className="site-container">
@@ -23,9 +26,31 @@ function App() {
 
         <div className='arr'>{arrow}</div>
         <span className='print'>print</span>
-        <div className='details'>{desk.Name}<br></br>{desk.employeeid} </div>
+        <div className='details'><b>{desk.Name}</b><br></br>{desk.employeeid} </div>
+        
+        
         
       </div>
+      
+      <div className='middle-content'>
+        <div><b>Appointment: </b>{desk.appointment}</div>
+        <div><b>Email: </b>{desk.Email}</div>
+        <div><b>phone: </b>{desk.phone}</div>
+      </div>
+
+      <div className='Main-content'>
+        <div><b>Status:</b><br></br>{desk.status}</div>
+        <div><b>Status:</b><br></br>{desk.status}</div>
+        <div><b>Status:</b><br></br>{desk.status}</div>
+      </div>
+
+      <div className='product'>
+        <div ><img className='checkbox' src={unchecked}></img></div>
+        <div ><img className='avatar' src="https://www.w3schools.com/howto/img_avatar.png"></img></div>
+        <div className='data1'><h2>sir lorem epsum hello</h2><span>hdfghdgdfsghfsdghdfgfdsghfs</span></div>
+
+      </div>
+
      </div>
   );
 }
---
+++ Assignment-3/code/src/customer-info.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+class Customer_info extends React.Component{
+    
+    render(){
+        return(
+            <>
+            <div className='middle content'>
+                <h1> hellow</h1>
+            </div>
+            </>
+        )
+
+    }
+}
+export default Customer_info
